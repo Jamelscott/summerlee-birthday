970ce39e4f17f20e244c192d26bb5cb35ee46190
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-05-03 00:10:59 -0700

    added birthday music

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import Form from './Form';
 import './App.css';
 import { supabase } from './client.js';
 import Intro from './Intro';
+import music from './assets/happybirthdaysong.mp3'
+import musicNotes from './assets/music.png'
+import musicNotesOff from './assets/stop_music.png'
 
 function App() {
   // State that holds an array of all posts
   const [posts, setPosts] = useState([]);
+  const [toggleMusic, setToggleMusic] = useState(true)
 
   //rerender the page when the posts state gets updated
   useEffect(() => {
     getData();
   }, [posts]);
 
+  useEffect(()=>{
+
+    console.log("playing")
+    if(toggleMusic){
+
+      const x = document.getElementById("audio").play()
+      
+    } else{
+      
+      const x = document.getElementById("audio").pause()
+    }
+
+  },[toggleMusic])
+
+  const turnOnOffMusic = ()=>{
+    setToggleMusic(!toggleMusic)
+  }
   // get request from the db with all posts
   const getData = async () => {
     const { data, error } = await supabase.from('Posts').select();
@@ -36,6 +57,10 @@ function App() {
 
   return (
     <div className="container">
+      <img onClick={turnOnOffMusic} alt="music notes" src={toggleMusic ? musicNotes : musicNotesOff} className="musicControl"/>
+      <audio id="audio" alt="happy birthday music">
+        <source src={music} type="audio/mp3"/>
+      </audio>
       <Intro />
       {posts ? mappedPosts : <></>}
       <Form posts={posts} getData={getData} />
@@ -44,3 +69,9 @@ function App() {
 }
 
 export default App;
+
+
+// It's Your Birthday! by Monk Turner + Fascinoma 
+// Music promoted by https://www.chosic.com/free-music/all/
+// Licensed under Creative Commons: Attribution 3.0 Unported (CC BY 3.0)
+// https://creativecommons.org/licenses/by/3.0/ 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import musicNotes from './assets/music.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
+
 root.render(
   <React.StrictMode>
     <div style={{display: "flex", justifyContent: "center"}}>
-
     <App />
     </div>
   </React.StrictMode>
